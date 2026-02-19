8959e2b09309c52bf67b639a6d23d3fc9dd753fc
Keerthana K <keerthanak8006@gmail.com>
2026-02-19 21:14:34 +0530

    basic practice

--- basic.py
+++ basic.py
@@ -14,3 +14,40 @@ print("division: ",a/b)
 a = 10
 print("Square: ",a**2)
 print("Cube: ",a**3)
+
+a = int(input("Enter a number: "))
+if a%2 == 0:
+  print("Even")
+else:
+  print("Odd")
+
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+if (a>=b) and (a>=c):
+  largest = a
+elif(b>=a) and (b>=c):
+  largest = b
+else:
+   largest = c
+print("Largest number is: ",largest)
+
+a = int(input("Enter a number: "))
+if a > 0:
+  print("Positive umber")
+elif a<0:
+  print("Negative number")
+else:
+  print("Zero")
+  print("Entered number is: ", a)
+
+cel = float(input("Enter a celsius: "))
+fahr = (cel*9/5)+32
+print("fahrenheit degree is: ",fahr) 
+
+
+
+
+
+    
