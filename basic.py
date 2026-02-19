17171e9277b8d29897116d9336a742520ccbea45
Keerthana K <keerthanak8006@gmail.com>
2026-02-19 19:23:38 +0530

    basic operation

---
+++ basic.py
@@ -0,0 +1,16 @@
+print("Keerthana.k , ECE,  8.5 ")
+
+age = int(input("Enter your age:  "))
+after = age + 5
+print("After five years the will be: ",after )
+
+a = 10
+b = 5
+print("sum: ", a+b)
+print("difference: ",a-b)
+print("product: ", a*b)
+print("division: ",a/b)
+
+a = 10
+print("Square: ",a**2)
+print("Cube: ",a**3)
