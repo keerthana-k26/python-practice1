97c0a98fa29061dede730ab6cfe1dd52a4853794
Keerthana K <keerthanak8006@gmail.com>
2026-02-21 16:41:07 +0530

    practice

---
+++ basic2.py
@@ -0,0 +1,17 @@
+numbers = [10,20,30,40,50]
+print(numbers)
+
+n= [ 1,2,3,4,5]
+print(n[1])
+
+n =[1,2,3,4,5]
+print(n[-1])
+
+n= [3,4,5,6]
+print(len(n))
+
+n = [ 4,5,6,7,8]
+n.append(9)
+print(n)
+n.remove(5)
+print(n)
