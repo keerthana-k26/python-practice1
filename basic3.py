801013335ce388bd60da192ee122dbb9a3b9d9f7
Keerthana K <keerthanak8006@gmail.com>
2026-02-22 20:32:09 +0530

    largest

--- basic3.py
+++ basic3.py
@@ -6,7 +6,7 @@ for num in numbers:
 print("The largest number is : ",largest)
 
 numbers =[30,42,56,78]
-largets = numbers[3] #
+largets = numbers[0] #
 for num in numbers:
    if num>largest:
       largest = num
