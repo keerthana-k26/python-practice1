9fdd2608b86e0e99bfc8e3f7a93081ead8e6e8a9
Keerthana K <keerthanak8006@gmail.com>
2026-02-23 18:27:36 +0530

    list

---
+++ 5.py
@@ -0,0 +1,10 @@
+coffee = ["Dark coffee" , "milk coffee", "hot coffee" , "black coffee", "lava coffee"]
+print(coffee.append("green coffee"))
+print(coffee) 
+
+coffee.insert(1 , "cold coffeee")
+print(coffee)
+
+coffee.pop(2)
+print(coffee)
+
