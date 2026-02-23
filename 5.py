3e5a1b20606e02ee29eb5765ea3c951922e4ccac
Keerthana K <keerthanak8006@gmail.com>
2026-02-23 19:03:44 +0530

    listss

--- 5.py
+++ 5.py
@@ -14,3 +14,24 @@ print(sorted(number))
 number.reverse()
 print(number)
 print(number.index(28))
+#test
+cumber = [3, 6, 7,2 ,4,5,8 ]
+largest = cumber[0]
+for num in cumber:
+    if num > largest:
+         largest = num
+print("The largest number is: ",largest)
+
+nums = [4, 7, 2, 7, 9, 2, 7]
+print(nums.count(7))
+
+nums = [4, 7, 2, 7, 9, 2, 7]
+count = 0
+for num in nums:
+     if num == 7:
+          count += 1
+print(count)
+
+nums.reverse()
+print(nums)
+     
